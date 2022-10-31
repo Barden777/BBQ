29ae2185a0e6e9a329f5d72cd07d069044610604
Barden777 <105597467+Barden777@users.noreply.github.com>
2022-10-31 15:29:01 +0200

    Add files via upload
    First commit

---
+++ js/script.js
@@ -0,0 +1,151 @@
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    if (menuArrows.length > 0) {
+        for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener('click', function (e) {
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+
+    }
+} else {
+    document.body.classList.add('_pc');
+}
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
+
+
+
+// Прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.toggle('_lock');
+                iconMenu.classList.toggle('_active');
+                menuBody.classList.toggle('_active');
+            }
+
+
+            window: scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
+window.addEventListener('scroll', e => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.page',
+    content: '.content'
+})
+
+// Показать/Спрятать Sidebar 
+const hideSideNav = document.querySelector('.hide__side__nav');
+const btnSideNavs = document.querySelectorAll('.btn__side__nav');
+const sideNav = document.querySelector('.side__nav');
+if (hideSideNav) {
+    hideSideNav.addEventListener("click", function (e) {
+        hideSideNav.classList.toggle('_unhide');
+        sideNav.classList.toggle('_hide');
+    })
+
+
+}
+
+$('.sldr__bbq').slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0px',
+    initialSlide: 2,
+    infinite: false,
+    responsive: [
+        {
+            breakpoint: 1630,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
